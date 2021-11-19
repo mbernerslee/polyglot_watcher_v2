f285dd07f6922eb90145d4bae523cce52b45defe
Matthew Berners-Lee <matthewbernerslee@gmail.com>
2021-11-19 12:39:33 +0000

    ran mix new

---
+++ lib/polyglot_watcher_v2.ex
@@ -0,0 +1,18 @@
+defmodule PolyglotWatcherV2 do
+  @moduledoc """
+  Documentation for `PolyglotWatcherV2`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PolyglotWatcherV2.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
