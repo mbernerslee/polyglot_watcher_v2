603e4b4524e9e437c5f4e1906a2a653915e6ab93
Matthew Berners-Lee <matthewbernerslee@gmail.com>
2022-10-14 17:06:04 +0100

    some kind of progress

--- lib/elixir/failed_test_action_chain.ex
+++ lib/elixir/failed_test_action_chain.ex
@@ -5,11 +5,12 @@ defmodule PolyglotWatcherV2.Elixir.FailedTestActionChain do
     %{{:mix_test_puts, 0} => %Action{runnable: :noop, next_action: next_action}}
   end
 
-  def build(failures, fail_action, next_action) do
-    build(%{}, 0, failures, fail_action, next_action)
+  def build([{first_file, _} | _] = failures, fail_action, next_action) do
+    failures = generate_test_plan(failures)
+    build({%{}, first_file}, 0, failures, fail_action, next_action)
   end
 
-  defp build(test_actions, index, [{test_path, line_number}], _fail_action, next_action) do
+  defp build({test_actions, _}, index, [{test_path, line_number}], _fail_action, next_action) do
     test_actions
     |> Map.merge(puts(index, test_path, line_number))
     |> Map.merge(mix_test(index, test_path, line_number, next_action))
@@ -29,6 +30,47 @@ defmodule PolyglotWatcherV2.Elixir.FailedTestActionChain do
       )
 
     build(test_actions, next_index, rest, fail_action, next_action)
+  end
+
+  defp generate_test_plan([]) do
+    []
+  end
+
+  defp generate_test_plan([first | rest] = failures) do
+    sorted_failures = sort_by_first(first, rest)
+
+    generate_first_file_test_plan(first, sorted_failures)
+
+    raise "oops"
+  end
+
+  defp generate_first_file_test_plan({file, line}, sorted_failures) do
+    count = Enum.count(sorted_failures, fn {this_file, _} -> this_file == file end)
+
+    case count do
+      1 -> ["#{file}:#{line_number}"]
+      _ -> ["#{file}:#{line_number}" | do_generate_first_file_test_plan(count - 1, file)]
+    end
+  end
+
+  defp do_generate_first_file_test_plan(count, file) do
+    do_generate_first_file_test_plan([], count, file)
+  end
+
+  defp do_generate_first_file_test_plan(count, file) do
+  end
+
+  defp sort_by_first({first_file, _} = first, rest) do
+    Enum.sort([first | rest], fn
+      {^first_file, _}, {_file_2, _} ->
+        true
+
+      {_file_1, _}, {^first_file, _} ->
+        false
+
+      {file_1, _}, {file_2, _} ->
+        file_1 <= file_2
+    end)
   end
 
   defp puts(index, test_path, line_number) do
--- test/elixir/failed_test_action_chain_test.exs
+++ test/elixir/failed_test_action_chain_test.exs
@@ -46,5 +46,146 @@ defmodule PolyglotWatcherV2.Elixir.FailedTestActionChainTest do
                  :zanzibar
                )
     end
+
+    test "given a list of many failures, returns actions whereby exponentially more tests are run" do
+      assert %{
+               {:mix_test_puts, 0} => %Action{
+                 runnable: {:puts, :magenta, "Running mix test test/a_test.exs:10"},
+                 next_action: {:mix_test, 0}
+               },
+               {:mix_test, 0} => %Action{
+                 runnable: {:mix_test, "test/a_test.exs:10"},
+                 next_action: %{0 => {:mix_test_puts, 1}, :fallback => :oh_noes}
+               },
+               {:mix_test_puts, 1} => %Action{
+                 runnable:
+                   {:puts, :magenta, "Running mix test test/a_test.exs --failed --max-cases 2"},
+                 next_action: {:mix_test, 1}
+               },
+               {:mix_test, 1} => %Action{
+                 runnable: {:mix_test, "test/a_test.exs --failed --max-cases 2"},
+                 next_action: %{0 => {:mix_test_puts, 2}, :fallback => :oh_noes}
+               },
+               {:mix_test_puts, 2} => %Action{
+                 runnable:
+                   {:puts, :magenta, "Running mix test test/a_test.exs --failed --max-cases 4"},
+                 next_action: {:mix_test, 2}
+               },
+               {:mix_test, 2} => %Action{
+                 runnable: {:mix_test, "test/a_test.exs --failed --max-cases 4"},
+                 next_action: %{0 => {:mix_test_puts, 3}, :fallback => :oh_noes}
+               },
+               {:mix_test_puts, 3} => %Action{
+                 runnable:
+                   {:puts, :magenta, "Running mix test test/a_test.exs --failed --max-cases 8"},
+                 next_action: {:mix_test, 3}
+               },
+               {:mix_test, 3} => %Action{
+                 runnable: {:mix_test, "test/a_test.exs --failed --max-cases 8"},
+                 next_action: %{0 => {:mix_test_puts, 4}, :fallback => :oh_noes}
+               },
+               {:mix_test_puts, 4} => %Action{
+                 runnable: {:puts, :magenta, "Running mix test test/a_test.exs --failed"},
+                 next_action: {:mix_test, 4}
+               },
+               {:mix_test, 4} => %Action{
+                 runnable: {:mix_test, "test/a_test.exs --failed"},
+                 next_action: %{0 => {:mix_test_puts, 5}, :fallback => :oh_noes}
+               },
+               {:mix_test_puts, 5} => %Action{
+                 runnable: {:puts, :magenta, "Running mix test test/b_test.exs --failed"},
+                 next_action: {:mix_test, 5}
+               },
+               {:mix_test, 5} => %Action{
+                 runnable: {:mix_test, "test/b_test.exs --failed"},
+                 next_action: %{0 => {:mix_test_puts, 6}, :fallback => :oh_noes}
+               },
+               {:mix_test_puts, 6} => %Action{
+                 runnable:
+                   {:puts, :magenta, "Running mix test test/c_test.exs test/d_test.exs--failed"},
+                 next_action: {:mix_test, 6}
+               },
+               {:mix_test, 6} => %Action{
+                 runnable: {:mix_test, "test/c_test.exs test/d_test.exs--failed"},
+                 next_action: %{0 => {:mix_test_puts, 7}, :fallback => :oh_noes}
+               },
+               {:mix_test_puts, 7} => %Action{
+                 runnable:
+                   {:puts, :magenta,
+                    "Running mix test test/e_test.exs test/f_test.exs test/g_test.exs --failed"},
+                 next_action: {:mix_test, 7}
+               },
+               {:mix_test, 7} => %Action{
+                 runnable: {:mix_test, "asssssss"},
+                 next_action: %{0 => :zanzibar, :fallback => :oh_noes}
+               }
+             } ==
+               FailedTestActionChain.build(
+                 [
+                   {"test/a_test.exs", 10},
+                   {"test/a_test.exs", 11},
+                   {"test/a_test.exs", 12},
+                   {"test/a_test.exs", 13},
+                   {"test/a_test.exs", 14},
+                   {"test/a_test.exs", 15},
+                   {"test/a_test.exs", 16},
+                   {"test/a_test.exs", 17},
+                   {"test/a_test.exs", 18},
+                   {"test/a_test.exs", 19},
+                   {"test/a_test.exs", 191},
+                   {"test/a_test.exs", 192},
+                   {"test/a_test.exs", 193},
+                   {"test/a_test.exs", 194},
+                   {"test/a_test.exs", 195},
+                   {"test/b_test.exs", 20},
+                   {"test/c_test.exs", 30},
+                   {"test/d_test.exs", 40},
+                   {"test/e_test.exs", 50},
+                   {"test/f_test.exs", 60},
+                   {"test/g_test.exs", 70}
+                 ],
+                 :oh_noes,
+                 :zanzibar
+               )
+    end
+
+    test "sorts failures by file" do
+      assert %{
+               {:mix_test_puts, 0} => %Action{
+                 runnable: {:puts, :magenta, "Running mix test test/a_test.exs:1"},
+                 next_action: {:mix_test, 0}
+               },
+               {:mix_test, 0} => %Action{
+                 runnable: {:mix_test, "test/a_test.exs:1"},
+                 next_action: %{0 => {:mix_test_puts, 1}, :fallback => :oh_noes}
+               },
+               {:mix_test_puts, 1} => %Action{
+                 runnable: {:puts, :magenta, "Running mix test test/a_test.exs --failed"},
+                 next_action: {:mix_test, 1}
+               },
+               {:mix_test, 1} => %Action{
+                 runnable: {:mix_test, "test/b_test.exs --failed"},
+                 next_action: %{0 => {:mix_test_puts, 2}, :fallback => :oh_noes}
+               },
+               {:mix_test_puts, 2} => %Action{
+                 runnable: {:puts, :magenta, "Running mix test test/b_test.exs --failed"},
+                 next_action: {:mix_test, 2}
+               },
+               {:mix_test, 2} => %Action{
+                 runnable: {:mix_test, "test/b_test.exs --failed"},
+                 next_action: %{0 => :zanzibar, :fallback => :oh_noes}
+               }
+             } ==
+               FailedTestActionChain.build(
+                 [
+                   {"test/a_test.exs", 1},
+                   {"test/b_test.exs", 2},
+                   {"test/a_test.exs", 3},
+                   {"test/a_test.exs", 4}
+                 ],
+                 :oh_noes,
+                 :zanzibar
+               )
+    end
   end
 end
